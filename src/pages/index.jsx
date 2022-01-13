80e39e862e60b2765519e15f8b9df54332ab674b
cyfinfaza <42043351+cyfinfaza@users.noreply.github.com>
2022-01-13 16:00:18 -0500

    basic sections and header layout

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,22 @@ import * as pageStyle from "./index.module.scss";
 const IndexPage = () => {
 	return (
 		<main className={pageStyle.container}>
-			<h1>ridgehacks</h1>
+			<div className={pageStyle.header}>
+				<p>logo</p>
+				<p>links</p>
+			</div>
+			<div
+				className={pageStyle.section}
+				style={{ backgroundColor: "#233", color: "white" }}
+			>
+				<h1>this is a section</h1>
+			</div>
+			<div className={pageStyle.section}>
+				<h1>this is a section</h1>
+			</div>
+			<div className={pageStyle.section}>
+				<h1>this is a section</h1>
+			</div>
 		</main>
 	);
 };
